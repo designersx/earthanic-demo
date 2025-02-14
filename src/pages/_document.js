47446a55d98dbffed97474b2cac5e9722cb7be0f
shubhamkumardx <shubhamkumar1.dx@gmail.com>
2025-02-14 14:33:24 +0530

    14-2-2025 v1

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="shortcut icon" href="images/favicon.png" />
+        <title>Earthenic Demo</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
