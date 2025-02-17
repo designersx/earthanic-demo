f5cfe0bc5f2e804aa2d9f2785db2282b0be83784
shubhamkumardx <shubhamkumar1.dx@gmail.com>
2025-02-17 16:51:53 +0530

    17-2-2025 v8

--- src/components/AddtoCart/Cart.js
+++ src/components/AddtoCart/Cart.js
@@ -66,12 +66,13 @@ const getProduct = () => {
   // Total price calculation
   const calculateTotal = () => {
     return cartItems.reduce((total, item) => {
-      return total + item.quantity * item.price; // Multiply quantity with price f 
+      return total + item.quantity * item.price; // Multiply quantity with price f f
     }, 0);
   };
 
   const totalPrice = calculateTotal();
 
+  
   return (
     <Offcanvas
       className={styles.OffcanvasMain}
