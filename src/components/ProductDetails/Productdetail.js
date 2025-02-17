2c1a139dac2625fdadcfa3789913560ed4589212
shubhamkumardx <shubhamkumar1.dx@gmail.com>
2025-02-17 14:45:59 +0530

    17-2-2025 v1

--- src/components/ProductDetails/Productdetail.js
+++ src/components/ProductDetails/Productdetail.js
@@ -3,22 +3,27 @@ import styles from "./Productdetail.module.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "react-bootstrap/Button";
 import Offcanvas from "react-bootstrap/Offcanvas";
+import CartOffcanvas from "../AddtoCart/Cart";
 
 const ProductDetails = ({ data }) => {
-  const [show, setShow] = useState(false);
-  const [quantity, setQuantity] = useState(1);
+  const [cartItems, setCartItems] = useState([]);
+  const [showCart, setShowCart] = useState(false);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
-  console.log("data----", data);
+  const handleClose = () => setShowCart(false);
+  const handleShow = () => {
+    const existingCartItems =
+      JSON.parse(localStorage.getItem("cartItems")) || [];
+    const updatedCartItems = [...existingCartItems, data];
+    localStorage.setItem("cartItems", JSON.stringify(updatedCartItems));
+    setCartItems(updatedCartItems);
+    setShowCart(true);
+  };
 
 
   return (
     <section>
       <div className={styles.ProductDetails}>
         {data.map((item) => {
-          console.log(item);
           return (
             <>
               <div className={styles.ProductImg}>
@@ -67,73 +72,12 @@ const ProductDetails = ({ data }) => {
 
               {/* OFFCANVAS */}
 
-              <Offcanvas
-                className={styles.OffcanvasMain}
-                show={show}
-                onHide={handleClose}
-                placement="end"
-              >
-                <Offcanvas.Header closeButton>
-                  <Offcanvas.Title>My Cart</Offcanvas.Title>
-                </Offcanvas.Header>
-                <Offcanvas.Body>
-                  <div className={styles.cartContent}>
-                    {/* Static Product */}
-                    <div className={styles.cartItem}>
-                      <div className={styles.removeButton}>
-                        <p>X</p>
-                      </div>
-                      <img
-                        src={item?.image}
-                        alt="Acme Slip-On Shoes"
-                        className={styles.productImage}
-                      />
-                      <div className={styles.details}>
-                        <p>{item?.title}</p>
-                        <p className={styles.price}>{`$${item?.price}USD`}</p>
-                        {/* <div className={styles.quantity}>
-                          <button>-</button>
-                          <span>4</span>
-                          <button>+</button>
-                        </div> */}
-                        <div className={styles.quantity}>
-                          <button
-                            onClick={() =>
-                              setQuantity((prev) => (prev > 1 ? prev - 1 : 1))
-                            }
-                          >
-                            -
-                          </button>
-                          <span>{quantity}</span>
-                          <button
-                            onClick={() => setQuantity((prev) => prev + 1)}
-                          >
-                            +
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className={styles.cartProduct}></div>
-
-                  <div className={styles.footer}>
-                    <div className={styles.summary}>
-                      <p>
-                        Taxes: <span>$0.00 USD</span>
-                      </p>
-                      <p>
-                        Shipping: <span>Calculated at checkout</span>
-                      </p>
-                      <p>
-                        Total: <span>$180.00 USD</span>
-                      </p>
-                    </div>
-                    <button className={styles.checkoutButton}>
-                      Proceed to Checkout
-                    </button>
-                  </div>
-                </Offcanvas.Body>
-              </Offcanvas>
+              <CartOffcanvas
+                show={showCart}
+                handleClose={handleClose}
+                // cartItems={cartItems}
+              />
+            
             </>
           );
         })}
