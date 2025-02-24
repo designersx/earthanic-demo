acfd16b82de3182e351163a10ffe58cc6e5792e6
shubhamkumardx <shubhamkumar1.dx@gmail.com>
2025-02-24 17:58:58 +0530

    24-2-2025 v2

--- src/components/AddtoCart/Cart.js
+++ src/components/AddtoCart/Cart.js
@@ -8,10 +8,11 @@ import {
   removeCartItem,
   updateCartItem,
 } from "@/lib/api";
+import { useFormState } from "react-dom";
 // import Loader from "../Loader/Loader";
 // import { debounce } from "lodash";
 
-const CartOffcanvas = ({ show, handleClose, cartItemsdet, addToCartData }) => {
+const CartOffcanvas = ({ show, handleClose, cartItemsdet, addToCartData, setCartCount= null  }) => {
   const [loading, setLoading] = useState(false); // New loading state
   const [cartItemss, setCartItemss] = useState([]);
   const [totalAmount, setTotalAmount] = useState(0);
@@ -19,13 +20,11 @@ const CartOffcanvas = ({ show, handleClose, cartItemsdet, addToCartData }) => {
   const [quantities, setQuantities] = useState({});
   const [totalTax, setTotalTax] = useState(0);
   const [isLoading, setIsLoading] = useState(false); // Loader state
-  const [getloading, setgetLoading] = useState(false);
-
 
 
   useEffect(() => {
     getCartDetails();
-  }, [cartItemsdet, addToCartData]);
+  }, [cartItemsdet, addToCartData, show]);
 
   useEffect(() => {
     getCartDetails();
@@ -35,42 +34,34 @@ const CartOffcanvas = ({ show, handleClose, cartItemsdet, addToCartData }) => {
   const getCartDetails = async () => {
     const cartId = localStorage.getItem("cartId");
     if (!cartId) return console.error("No Cart ID found!");
+    setLoading(true);
     try {
       const cartResponse = await getCartList(cartId);
       const res = JSON.parse(cartResponse?.data);
 
       const checkoutLink = res?.data?.cart?.checkoutUrl || "";
       setCheckoutUrl(checkoutLink);
-
-
-      // const items =
-      //   res?.data?.cart?.lines?.edges?.map((item) => ({
-      //     title: item?.node?.merchandise?.product?.title || "No Title",
-      //     imageUrl: item?.node?.merchandise?.image?.url || "",
-      //     amount: item?.node?.merchandise?.price?.amount || "0",
-      //     quantity: item?.node?.quantity || 1,
-      //     id: item?.node?.id,
-      //   })) || [];
-
       const items =
-      res?.data?.cart?.lines?.edges?.map((item) => {
-        const selectedSize =
-          item?.node?.merchandise?.selectedOptions?.find(
-            (option) => option.name === "Size"
-          )?.value || "No Size";
-    
-        return {
-          title: item?.node?.merchandise?.product?.title || "No Title",
-          imageUrl: item?.node?.merchandise?.image?.url || "",
-          amount: item?.node?.merchandise?.price?.amount || "0",
-          quantity: item?.node?.quantity || 1,
-          id: item?.node?.id,
-          size: selectedSize, // Size ko add kar diya object me
-        };
-      }) || [];
- 
+        res?.data?.cart?.lines?.edges?.map((item) => {
+          const selectedSize =
+            item?.node?.merchandise?.selectedOptions?.find(
+              (option) => option.name === "Size"
+            )?.value || "No Size";
+
+          return {
+            title: item?.node?.merchandise?.product?.title || "No Title",
+            imageUrl: item?.node?.merchandise?.image?.url || "",
+            amount: item?.node?.merchandise?.price?.amount || "0",
+            quantity: item?.node?.quantity || 1,
+            id: item?.node?.id,
+            size: selectedSize, // Size ko add kar diya object me
+          };
+        }) || [];
+
       setCartItemss(items);
-    
+      let abc = items?.length
+      setCartCount = abc  
+
       // Initialize individual quantities
       const initialQuantities = {};
       items.forEach((item) => {
@@ -84,6 +75,7 @@ const CartOffcanvas = ({ show, handleClose, cartItemsdet, addToCartData }) => {
     } catch (error) {
       console.error("Error fetching cart details:", error);
     } finally {
+      setLoading(false);
     }
   };
 
@@ -94,7 +86,7 @@ const CartOffcanvas = ({ show, handleClose, cartItemsdet, addToCartData }) => {
     setLoading(true);
     try {
       const response = await removeCartItem(cartId, lineId);
-    
+
       await getCartDetails();
     } catch (error) {
       console.error("Error fetching cart details:", error);
@@ -166,7 +158,9 @@ const CartOffcanvas = ({ show, handleClose, cartItemsdet, addToCartData }) => {
                   <div className={styles.details}>
                     <p>{item?.title}</p>
                     <p className={styles.price}>{`$${item?.amount} USD`}</p>
-                    <p className={styles.price}>{`${item?.size}`}</p>
+                    <p className={styles.price}>
+                      <b>size:</b> {`${item?.size}`}
+                    </p>
                     <div className={styles.quantity}>
                       <button onClick={() => handleDecrement(item.id)}>
                         -
